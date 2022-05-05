356562ca6af94e602eba48cfa1fd103e31fe9532
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-05-05 17:30:01 +0200

    stile e linee su highcharts

--- quasar-dashboard/src/boot/highcharts.js
+++ quasar-dashboard/src/boot/highcharts.js
@@ -2,10 +2,20 @@ import { boot } from 'quasar/wrappers'
 import Highcharts from 'highcharts'
 import Stock from "highcharts/modules/stock";
 import HighchartsVue from 'highcharts-vue'
+import exportingInit from 'highcharts/modules/exporting'
+import exportingDataInit from 'highcharts/modules/export-data'
+import darkUnica from "highcharts/themes/dark-unica";
+import darkGreen from "highcharts/themes/dark-green";
+import highContrastDark from "highcharts/themes/high-contrast-dark"
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async ( { app }) => {
   // something to do
   Stock(Highcharts)
+  exportingInit(Highcharts)
+  exportingDataInit(Highcharts)
+  // darkUnica(Highcharts);
+  // darkGreen(Highcharts)
+  // highContrastDark(Highcharts)
   app.use(HighchartsVue)
 })
--- quasar-dashboard/src/components/charts/highcharts/D03_highchart.vue
+++ quasar-dashboard/src/components/charts/highcharts/D03_highchart.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- <h1>HIGHCHART STOCK D' 0.3 mm</h1>-->
-  >
-  <highcharts :constructor-type="'stockChart'" :options="chartOptions"></highcharts>
+
+  <highcharts :constructor-type="'stockChart'" :options="chartOptions"/>
 </template>
 
 <script setup>
@@ -26,7 +26,29 @@ const chartOptions = reactive({
       data: chartData.data
 
     },
-  ]
+  ],
+  yAxis: {
+    title: {
+      // text: 'd\''
+    },
+    min: 0,
+    max: 10,
+    plotLines:[
+      {
+      value: 0.5,
+      color: '#099D0D',
+      dashStyle: 'shortdash',
+      width: 2,
+      label: {
+              text: 'baseline',
+              align: 'right',
+              // style: {
+              //      color: "white"
+              //     }
+             }
+      }
+    ]
+  }
 })
 
 const props = defineProps( {
@@ -38,12 +60,35 @@ onMounted(()=>{
 
   console.log("MOUNTED HIGHCHARTS D03")
   console.log("dataset data: ",props.apiData)
-  const data = props['apiData'].map(entry => [new Date(entry.date).setHours(0,0,0,0),Number(entry.D03)])
+  const data = props['apiData'].map(entry => [new Date(entry.date).setHours(1,0,0,0,),Number(entry.D03)])
   console.log("date: ",data)
   chartData.data = data
   chartOptions.series[0].data=chartData.data
   console.log("chartData data: ",chartData.data)
   console.log("chartOptions: ",chartOptions)
+  const baselines= props['apiData'].filter(entry=>entry.baseline===true)
+  console.log("BASELINES FOR D03: ",baselines)
+    const sortedBaselines = baselines.sort((a,b)=>{
+         return new Date(b.date) - new Date(a.date)
+       })
+  const baselineD03 = baselines.length ?  Number(sortedBaselines[0].D03) : 0
+  const d03Data =  props['apiData'].map(entry => Number(entry.D03))
+
+  const yMax  =Math.max(...d03Data)
+  const yMin  =Math.min(...d03Data)
+
+  const upper = baselines ? baselineD03*1.2 : 0
+
+  const lower = baselines ? baselineD03*0.8 : 0
+
+  const suggestedMax = yMax > upper ? yMax+0.1 : upper
+
+  const suggestedMin = yMin < lower ? yMin-0.1 : lower
+
+  chartOptions.yAxis.min = suggestedMin
+  chartOptions.yAxis.max = suggestedMax
+
+  chartOptions.yAxis.plotLines[0].value = baselineD03
 })
 
 </script>
