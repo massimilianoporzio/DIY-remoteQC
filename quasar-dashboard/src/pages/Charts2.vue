5a878796dd83a85da8bc3f964cd9b712cce58289
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-05-24 21:35:36 +0200

    PProvato oagina divsa in due come qaelum e poi unica. aggiunti i tab

---
+++ quasar-dashboard/src/pages/Charts2.vue
@@ -0,0 +1,92 @@
+<template>
+
+  <q-page class="q-pa-sm " style="height: 1px">
+    <div class="column full-height" >
+
+        <q-tabs dense v-model="tab"
+        align="left"
+        class="bg-primary text-white shadow-2"
+        :breakpoint="0">
+          <q-tab name="overview">Overview</q-tab>
+          <q-tab name="charts">Charts</q-tab>
+        </q-tabs>
+      <div class="bg-amber col" >
+
+           <q-tab-panels v-model="tab" class="bg-primary full-height"  >
+          <q-tab-panel name="overview" class="text-white bg-primary" >
+              Tabella
+          </q-tab-panel>
+
+          <q-tab-panel name="charts" class="text-white">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+
+        </q-tab-panels>
+
+
+      </div>
+
+
+<!--      <div class="col" >-->
+<!--        1 of 2 eeee-->
+<!--      </div>-->
+    </div>
+  </q-page>
+
+</template>
+
+<script>
+import {defineComponent, defineAsyncComponent} from 'vue'
+import axios from "axios";
+import {ref} from 'vue'
+export default defineComponent({
+  name: "Charts",
+  components: {
+    PieChart: defineAsyncComponent(() => import('components/charts/PieChart')),
+    ScatterPlot: defineAsyncComponent(() => import('components/charts/ScatterPlot')),
+    LineChart: defineAsyncComponent(() => import('components/charts/LineChart')),
+    BarChart: defineAsyncComponent(() => import('components/charts/BarChart')),
+    AreaChart: defineAsyncComponent(() => import('components/charts/AreaChart')),
+    GuageChart: defineAsyncComponent(() => import('components/charts/GuageChart')),
+    D03: defineAsyncComponent(()=>import('components/charts/highcharts/D03_highchart'))
+  },
+  data() {
+    return {
+      tab: ref('overview'),
+      emptyData: false,
+      loaded: false,
+        chartsData: []
+
+      }
+  },
+   methods:{
+     requestData () {
+      axios.get('http://localhost:3000/results')
+        .then(response => {
+          console.log("USED AXIOS ",response.data)
+          this.chartsData = response.data
+
+          this.loaded = true
+          if(!response.data.length){
+            this.emptyData = true
+            console.log("SORRY NO DATA FROM THE API")
+          }
+          console.log("LOADED - chartsData are in parent: ",this.chartsData)
+        }).catch(()=>{
+          this.loaded = false
+      })
+    },
+  },
+   async mounted() {
+    this.loaded = false
+    this.requestData()
+
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
