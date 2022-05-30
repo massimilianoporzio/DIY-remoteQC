4ea9614985889e5ff5891fb2e2b9bac988d08d23
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-05-30 22:23:26 +0200

    aggiornata query

--- server/api.js
+++ server/api.js
@@ -33,7 +33,12 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.get('/results', (req, res)=>{
     client.query(`SELECT id, date, "datetimeJava", "SIGNAL", "SNR", "SDNR", "MTF50", "MTF20", "MTF10", "D03", "D4", "mAs",
-baseline, "DI", "EI", "EIt", encode(signal_image, 'base64') as signal_image FROM public."dailyQC_dailyqc" ORDER BY date ASC;`, (err, result)=>{
+baseline, "DI", "EI", "EIt", encode(signal_image, 'base64') as signal_image,
+    "mAs", "DAP","CONTRAST",
+    comments, "esitoCONTRAST", "esitoD03", "esitoD4", "esitoMTF50", "esitoSDNR", "esitoSIGNAL", "esitoSNR",
+     "kV", "deltaCONTRAST", "deltaD03", "deltaD4", "deltaDAP", "deltaDI", "deltaEI", "deltaMAS", "deltaMTF50", 
+     "deltaSDNR", "deltaSIGNAL", "deltaSNR", "diffKV", "deltaMTF10", "deltaMTF20"
+FROM public."dailyQC_dailyqc" ORDER BY date ASC;`, (err, result)=>{
         if(!err){
             res.send(result.rows);
         }
